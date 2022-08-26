cb5e0fc8b0acbb11dd8f1d69ff0d462b85431614
Nicolas Roos <nicolas.k.roos@gmail.com>
2022-08-26 16:51:22 +0200

    feat(app): added tailwindcss

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,16 @@ const app = express()
 
 app.set('view engine', 'pug')
 app.use(express.json())
+app.use(express.static('./public'))
 
 app.use('/api/v1/comment', commentApi)
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', {
+        title: 'Hey',
+        message: 'Hello there!',
+        articles: ['test1', 'test3', 'test5'],
+    })
 })
 
 const server = app.listen(3000, () =>
