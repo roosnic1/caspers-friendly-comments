3a7f06398fb1757baf4a0a133acaa1d13da7c63f
Nicolas Roos <nicolas.k.roos@gmail.com>
2022-08-26 15:55:45 +0200

    feat(app): added pug as express template engine

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,14 @@ import commentApi from './commentApi'
 const prisma = new PrismaClient()
 const app = express()
 
+app.set('view engine', 'pug')
 app.use(express.json())
 
 app.use('/api/v1/comment', commentApi)
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
 
 const server = app.listen(3000, () =>
     console.log(`🚀 Server ready at: http://localhost:3000`)
