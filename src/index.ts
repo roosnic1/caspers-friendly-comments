32590749c8fd43c31f4df3b8b1c151814241ba52
Nicolas Roos <nicolas.k.roos@gmail.com>
2022-09-01 15:07:24 +0200

    chore(app): moved index.ts back to src

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import app from './src/app'
+import app from './app'
 
 app.listen(3000, process.env.NODE_ENV === 'development' ? '0.0.0.0' : '', () =>
     console.log(`🚀 Server ready at: http://localhost:3000`)
